1de6ba7a537c29eaacf9f9ed32b40d3bc56f58a3
Laci <mlacko020523@gmail.com>
2023-09-24 23:50:59 +0200

    Első :D

---
+++ undefined/App.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+app.use(express.static("static"))
+const pathJoin = (dir)  =>  {return path.join(__dirname, /static/+dir)}
+
+app.listen(3000)
+app.get('/', (req, res, next) =>{
+    res.sendFile(pathJoin("login.html"))
+})
+
+app.get("/gamemodes", (req, res, next) =>{
+    res.sendFile(pathJoin("gamemodes.html"))
+})
+
+app.post('/', (req, res, next) => {
+    res.redirect("/gamemodes")
+})
+
+app.get("/gamemodes/party", (req, res, next) =>{
+    res.sendFile(pathJoin("party.html"))
+})
+
+app.get("/profile", (req, res, next) =>{
+    res.sendFile(pathJoin("profile.html"))
+})
+
+
